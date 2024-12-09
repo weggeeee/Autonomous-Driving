f0abd9d56ad8045e1b8e78f92671be9846b0b329
weggeeee <94621041+weggeeee@users.noreply.github.com>
2024-12-09 21:46:53 +0100

    Update runTrafficSignClassifier.m

--- RCNN_TrafficSignDevelopment/runTrafficSignClassifier.m
+++ RCNN_TrafficSignDevelopment/runTrafficSignClassifier.m
@@ -15,13 +15,14 @@ while true
 %     Run Detection
     [bboxes,score,label] = detect(rcnn,img,'MiniBatchSize',128);
 
+% Set confidence threshold
     [score, idx] = max(score);
     if label(idx) == 'schoolSign' & score > 0.95
         schoolSign = 1;
     elseif label(idx) == 'stopSign' & score > 0.95
         stopSign = 1;
     end
-    
+% Write progress and result on console    
     stopSign
     schoolSign
     
